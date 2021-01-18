d0a85ad3e1626c2559bebc5e2f542e70bca5594a
Elinaldo Jr <jrdulahan@gmail.com>
2021-01-18 07:47:01 -0300

    validação form de contato

--- js/validate.js
+++ js/validate.js
@@ -771,7 +771,7 @@
     presence: function(value, options) {
       options = v.extend({}, this.options, options);
       if (options.allowEmpty !== false ? !v.isDefined(value) : v.isEmpty(value)) {
-        return options.message || this.message || "can't be blank";
+        return options.message || this.message || "não pode estar em branco";
       }
     },
     length: function(value, options, attribute) {
